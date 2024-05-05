5469a2e0d505849d2d192f56c1ae8fc59292714b
Honzoraptor <j.ackermann@email.cz>
2024-05-05 10:00:41 +0200

    Implemented an infinite scroll on the feed

--- src/lib/components/Feed.svelte
+++ src/lib/components/Feed.svelte
@@ -1,21 +1,42 @@
 <script lang="ts">
   import { supabase } from "$lib/supabaseClient";
   import type { DBPost } from "$lib/types/db";
+  import { onMount } from "svelte";
   import Post from "./Post.svelte";
+  import NoSearchResult from "./NoSearchResult.svelte";
+  import FeedEnd from "./FeedEnd.svelte";
 
-  let posts: DBPost[];
+  let posts: DBPost[] = [];
+  let isReachedFeedEnd = false;
 
   async function getData() {
-    const { data } = await supabase.from("posts").select();
-    data && (posts = data);
+    const { data } = await supabase
+      .from("posts")
+      .select()
+      .range(posts.length, posts.length + 2);
+
+    if (data) {
+      data.length > 0
+        ? (posts = [...posts, ...data])
+        : (isReachedFeedEnd = true);
+    }
   }
 
-  getData();
+  onMount(() => {
+    getData();
+    const onscroll = () => {
+      const scrolledTo = window.scrollY + window.innerHeight + 50;
+      const isReachBottom = document.body.scrollHeight <= scrolledTo;
+      console.log(scrolledTo, document.body.scrollHeight, isReachBottom);
+      isReachBottom && getData();
+    };
+    document.addEventListener("scroll", onscroll);
+  });
 </script>
 
 <main class="feed-main desktop-nav-margin">
   <div class="posts-inline-spacing">
-    {#if posts && posts.length > 0}
+    {#if posts.length > 0}
       {#each posts as { id, created_at, image_url, title, description, likes, user_id }}
         <Post
           {id}
@@ -28,6 +49,9 @@
           feedPost={true}
         />
       {/each}
+    {/if}
+    {#if isReachedFeedEnd}
+      <FeedEnd />
     {/if}
   </div>
   <div class="mobile-nav-placeholder"></div>
---
+++ src/lib/components/FeedEnd.svelte
@@ -0,0 +1,3 @@
+<div class="flex-center-all alpha-bg-element ab-app-dialog">
+  <p class="less text-center">Looks like you reached the very end!</p>
+</div>
