f2b7270ca66b1f16b48f22dc8af0cfb79c60ef67
Honzoraptor <j.ackermann@email.cz>
2024-05-12 09:00:53 +0200

    Updated types

--- src/lib/types/db.ts
+++ src/lib/types/db.ts
@@ -1,34 +1,36 @@
 export type DBUserData = {
-  bio: string,
-  displayed_username: string,
-  followers: string[],
-  follows: string[],
-  id: string,
-  image_url: string,
-  joined_at: number,
-  url_username: string
-}
+  bio: string;
+  displayed_username: string;
+  followers: string[];
+  follows: string[];
+  id: string;
+  image_url: string;
+  joined_at: number;
+  url_username: string;
+  blocked: string[];
+  saved: string[];
+};
 
 export type DBPost = {
-  created_at: number,
-  description: string,
-  id: string,
-  image_url: string,
-  likes: string[],
-  title: string,
-  user_id: string
-}
+  created_at: number;
+  description: string;
+  id: string;
+  image_url: string;
+  likes: string[];
+  title: string;
+  user_id: string;
+};
 
 export type DBComment = {
-  created_at: number,
-  id: string,
-  likes: string[],
-  post_id: string,
-  text: string,
-  user_id: string,
-  edited: boolean | null
-}
+  created_at: number;
+  id: string;
+  likes: string[];
+  post_id: string;
+  text: string;
+  user_id: string;
+  edited: boolean | null;
+};
 
 export interface DBReply extends DBComment {
-  comment_id: string
+  comment_id: string;
 }
