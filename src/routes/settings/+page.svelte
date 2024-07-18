66249dfebec00cd19928f1723078f39a0a62749d
Honzoraptor <j.ackermann@email.cz>
2024-07-18 08:28:40 +0200

    Added smth to settings

--- src/routes/settings/+page.svelte
+++ src/routes/settings/+page.svelte
@@ -27,6 +27,7 @@
       currUserDbData = val;
       username = val.url_username;
       displayedName = val.displayed_username;
+      bio = val.bio;
     }
   });
 
@@ -50,6 +51,13 @@
         error
           ? setNotification({ text: "Something went wrong" })
           : setNotification({ text: "Displayed name updated!" });
+      } else if (colName === "bio") {
+        const { error } = await supabase.from("users").update({
+          bio: bio,
+        });
+        error
+          ? setNotification({ text: "Something went wrong" })
+          : setNotification({ text: "Bio updated!" });
       }
     }
   }
@@ -61,57 +69,89 @@
 
 {#if currUserDbData}
   <div class="desktop-nav-padding settings-main-wrp">
-    <main class="settings-main sec-bg-element form-gap">
-      <h2 class="form-margin-bottom">Settings</h2>
+    <main class="settings-main sec-bg-element gap-20">
+      <h2>Settings</h2>
       <div class="settings-content">
-        <section class="settings-section gap-10 flex-column">
+        <section class="settings-section gap-20 flex-column" id="profile">
           <h3 class="settings-section-heading">Profile settings</h3>
           <div class="settings-grid">
-            <form
-              on:submit={() => {
-                editProfile("username");
-              }}
-              class="flex-between align-end gap-10 settings-form"
-            >
-              <FormElement
-                id="username"
-                labelClass="no-tp"
-                initLabel="Username"
-                placeholder="Username"
-                inputClass="settings-input-height"
-                label={usernameLabel}
-                bind:value={username}
-                isValid={usernameLabel === ""}
+            <div class="grid-wrp">
+              <img
+                src={currUserDbData.image_url}
+                alt="Profile"
+                class="rounded settings-user-image"
               />
-              <button
-                class="user-input button-element primary-button settings-input-height"
-                type="submit"
-                disabled={username === currUserDbData.url_username}>Save</button
+            </div>
+            <div class="gap-10 flex-column">
+              <form
+                on:submit={() => {
+                  editProfile("username");
+                }}
+                class="flex-between align-end gap-10 settings-form"
               >
-            </form>
-            <form
-              on:submit={() => {
-                editProfile("displayedName");
-              }}
-              class="flex-between align-end gap-10 settings-form"
-            >
-              <FormElement
-                id="displayed-name"
-                labelClass="no-tp"
-                initLabel="Displayed username"
-                inputClass="settings-input-height"
-                placeholder="Displayed name"
-                label={displayedNameLabel}
-                bind:value={displayedName}
-                isValid={displayedNameLabel === ""}
-              />
-              <button
-                class="user-input button-element primary-button settings-input-height"
-                type="submit"
-                disabled={displayedName === currUserDbData.displayed_username}
-                >Save</button
+                <FormElement
+                  id="username"
+                  labelClass="no-tp"
+                  initLabel="Username"
+                  placeholder="Username"
+                  inputClass="settings-input-height"
+                  label={usernameLabel}
+                  bind:value={username}
+                  isValid={usernameLabel === ""}
+                />
+                <button
+                  class="user-input button-element primary-button settings-input-height"
+                  type="submit"
+                  disabled={username === currUserDbData.url_username}
+                  >Save</button
+                >
+              </form>
+              <form
+                on:submit={() => {
+                  editProfile("displayedName");
+                }}
+                class="flex-between align-end gap-10 settings-form"
               >
-            </form>
+                <FormElement
+                  id="displayed-name"
+                  labelClass="no-tp"
+                  initLabel="Displayed username"
+                  inputClass="settings-input-height"
+                  placeholder="Displayed name"
+                  label={displayedNameLabel}
+                  bind:value={displayedName}
+                  isValid={displayedNameLabel === ""}
+                />
+                <button
+                  class="user-input button-element primary-button settings-input-height"
+                  type="submit"
+                  disabled={displayedName === currUserDbData.displayed_username}
+                  >Save</button
+                >
+              </form>
+              <form
+                on:submit={() => {
+                  editProfile("bio");
+                }}
+                class="flex-between align-end gap-10 settings-form"
+              >
+                <FormElement
+                  id="bio"
+                  initLabel="Bio"
+                  placeholder="Your bio"
+                  label={bioLabel}
+                  bind:value={bio}
+                  wrpClass="w-full"
+                  type="textarea"
+                  isValid={bioLabel === ""}
+                />
+                <button
+                  class="user-input button-element primary-button settings-input-height"
+                  type="submit"
+                  disabled={bio === currUserDbData.bio}>Save</button
+                >
+              </form>
+            </div>
           </div>
         </section>
       </div>
