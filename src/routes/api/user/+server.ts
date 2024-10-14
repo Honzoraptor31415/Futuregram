fccd254ea1bf82a2e71be92e06a48d1efd4075b4
Honzoraptor <j.ackermann@email.cz>
2024-10-14 12:33:17 +0200

    Added PATCH to the /api/user

--- src/routes/api/user/+server.ts
+++ src/routes/api/user/+server.ts
@@ -1,5 +1,5 @@
 import * as validation from "$lib/helper/formValidation";
-import { error, json, type RequestHandler } from "@sveltejs/kit";
+import { json, type RequestHandler } from "@sveltejs/kit";
 
 export const GET: RequestHandler = async ({ url, locals }) => {
   const supabase = locals.supabase;
@@ -34,7 +34,7 @@ export const POST: RequestHandler = async ({ locals, request }) => {
   const { user: authUser } = await locals.safeGetSession();
   const body = await request.json();
   const bodyKeys = Object.keys(body);
-  const expectedBodyLength = 2;
+  const expectedBodyLength = 3;
 
   if (!authUser) {
     return json({
@@ -150,5 +150,133 @@ export const POST: RequestHandler = async ({ locals, request }) => {
   return json({
     ok: true,
     message: "Data inserted succesfully",
+  });
+};
+
+export const PATCH: RequestHandler = async ({ locals, request }) => {
+  const supabase = locals.supabase;
+  const { user: authUser } = await locals.safeGetSession();
+  const body = await request.json();
+  const bodyKeys = Object.keys(body);
+  const maxBodyLength = 3;
+
+  const unupdatableColumns = [
+    "id",
+    "joined_at",
+    "image_url",
+    "following",
+    "followers",
+    "saved",
+    "blocked",
+    "rooms",
+    "last_seen",
+  ];
+
+  for (let i = 0; i < bodyKeys.length; i++) {
+    if (unupdatableColumns.includes(bodyKeys[i])) {
+      return json({
+        ok: false,
+        message: `Cannot update column ${bodyKeys[i]}`,
+      });
+    }
+  }
+
+  if (!authUser) {
+    return json({
+      ok: false,
+      message: "You have to be signed in",
+    });
+  } else if (bodyKeys.length === 0) {
+    return json({
+      ok: false,
+      message: "Please provide data",
+    });
+  } else if (bodyKeys.length > maxBodyLength) {
+    return json({
+      ok: false,
+      message: "Unexpected data",
+    });
+  }
+
+  const { urlUsername, displayedUsername, bio } = body;
+
+  const usernameCheck = await validation.usernameCheck(urlUsername);
+  const displayedUsernameCheck =
+    validation.displayedNameCheck(displayedUsername);
+  const bioCheck = validation.bioCheck(bio);
+
+  if (urlUsername && !usernameCheck.isValid) {
+    return json({
+      ok: false,
+      checks: {
+        username: {
+          isValid: usernameCheck.isValid,
+          message: usernameCheck.message,
+        },
+        displayedUsername: {
+          isValid: displayedUsernameCheck.isValid,
+          message: displayedUsernameCheck.message,
+        },
+        bio: {
+          isValid: bioCheck.isValid,
+          message: bioCheck.message,
+        },
+      },
+    });
+  } else if (displayedUsername && !displayedUsernameCheck.isValid) {
+    return json({
+      ok: false,
+      checks: {
+        username: {
+          isValid: usernameCheck.isValid,
+          message: usernameCheck.message,
+        },
+        displayedUsername: {
+          isValid: displayedUsernameCheck.isValid,
+          message: displayedUsernameCheck.message,
+        },
+        bio: {
+          isValid: bioCheck.isValid,
+          message: bioCheck.message,
+        },
+      },
+    });
+  } else if (bio && !bioCheck.isValid) {
+    return json({
+      ok: false,
+      checks: {
+        username: {
+          isValid: usernameCheck.isValid,
+          message: usernameCheck.message,
+        },
+        displayedUsername: {
+          isValid: displayedUsernameCheck.isValid,
+          message: displayedUsernameCheck.message,
+        },
+        bio: {
+          isValid: bioCheck.isValid,
+          message: bioCheck.message,
+        },
+      },
+    });
+  }
+
+  const { error: resError } = await supabase
+    .from("users")
+    .update({
+      body,
+    })
+    .eq("id", authUser.id);
+
+  if (resError) {
+    return json({
+      ok: false,
+      message: resError.message,
+    });
+  }
+
+  return json({
+    ok: true,
+    message: "Data updated succesfully",
   });
 };
